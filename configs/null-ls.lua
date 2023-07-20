810d36a23652137c425addf6aaa80ceb27f5b032
Arnold <rnldlinus@gmail.com>
2023-07-20 19:02:53 +0300

    enabled yaml formatting with prettier

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -22,7 +22,7 @@ local sources = {
 	-- webdev stuff
 	-- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
 	b.formatting.prettier.with({
-		filetypes = { "html", "markdown", "css", "vue", "svelte", "javascript", "typescript", "json" },
+		filetypes = { "html", "markdown", "css", "vue", "svelte", "javascript", "typescript", "json", "yaml" },
 	}), -- so prettier works only on these filetypes
 
 	-- Lua
